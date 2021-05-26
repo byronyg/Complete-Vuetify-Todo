d91bdd576bf8c3bb96a63874f96b70f54cd95213
Byron Yego <byronyego@gmail.com>
2021-05-26 11:53:19 +0300

    Added Delete Dialog

---
+++ src/components/Todo/Dialogs/DialogDelete.vue
@@ -0,0 +1,41 @@
+<template>
+  <v-dialog
+      :value="true"
+      persistent
+      max-width="290"
+    >
+      <v-card>
+        <v-card-title class="headline">
+          Are you sure you want to delete this item?
+        </v-card-title>
+        <v-card-text>By clicking yes you understand that the task will be removed from the database and this fuction is irreversible.</v-card-text>
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-btn
+            color="pink darken-1"
+            text
+            @click="dialog = false"
+          >
+            Disagree
+          </v-btn>
+          <v-btn
+            color="pink darken-1"
+            text
+            @click="dialog = false"
+          >
+            Agree
+          </v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
--- src/components/Todo/Task.vue
+++ src/components/Todo/Task.vue
@@ -26,11 +26,18 @@
               </template>
             </v-list-item>
             <v-divider></v-divider>
+
+            <dialog-delete />
         </div>
 </template>
 
 <script>
+import DialogDelete from './Dialogs/DialogDelete.vue'
 export default {
-    props: ['task']
+    props: ['task'],
+    components: {
+      'dialog-delete' : require('@/components/Todo/Dialogs/DialogDelete.vue').default
+    
+    }
 }
 </script>
