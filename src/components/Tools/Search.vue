9abd568db5b7d79d94e13c6c5800ca26426345e1
Byron Yego <byronyego@gmail.com>
2021-12-15 14:22:09 +0300

    styling search button

--- src/components/Tools/Search.vue
+++ src/components/Tools/Search.vue
@@ -1,10 +1,22 @@
 <template>
-  <v-text-field label="Filled" filled></v-text-field>
+  <v-text-field
+    class="expanding-search mt-1"
+    placeholder="Search"
+    filled
+    dense
+    clearable
+    prepend-inner-icon="mdi-magnify"
+  >
+  </v-text-field>
 </template>
 
 <script>
 export default {};
 </script>
 
-<style>
+<style lang="sass">
+.expanding-search
+    .v-input__slot
+        &:before, &:after
+            border-color: transparent !important
 </style>
