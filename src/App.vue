f35cde1af8ea86b3255908177e09901e6acf91a4
Byron Yego <byronyego@gmail.com>
2021-12-15 13:54:04 +0300

    small commit

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,17 @@
 <template>
   <v-app id="inspire">
-    <v-navigation-drawer
-    v-model="drawer"
-    app>
+    <v-navigation-drawer v-model="drawer" app>
       <v-list-item>
         <v-list-item-content>
-          <v-list-item-title class="title">
-            Create a List
-          </v-list-item-title>
-          <v-list-item-subtitle>
-            Best List Ever
-          </v-list-item-subtitle>
+          <v-list-item-title class="title"> Create a List </v-list-item-title>
+          <v-list-item-subtitle> Best List Ever </v-list-item-subtitle>
         </v-list-item-content>
       </v-list-item>
 
       <v-divider></v-divider>
 
-      <v-list
-        dense
-        nav
-      >
-        <v-list-item
-          v-for="item in items"
-          :key="item.title"
-          :to="item.to"
-          link
-        >
+      <v-list dense nav>
+        <v-list-item v-for="item in items" :key="item.title" :to="item.to" link>
           <v-list-item-icon>
             <v-icon>{{ item.icon }}</v-icon>
           </v-list-item-icon>
@@ -37,17 +23,9 @@
       </v-list>
     </v-navigation-drawer>
 
-    <v-app-bar
-      app
-      prominent
-        color="#E91E63"
-      dark
-    >
+    <v-app-bar app prominent color="#E91E63" dark>
       <template v-slot:img="{ props }">
-        <v-img
-          v-bind="props"
-          
-        ></v-img>
+        <v-img v-bind="props"></v-img>
       </template>
 
       <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
@@ -55,17 +33,9 @@
       <v-app-bar-title>Create a List</v-app-bar-title>
 
       <v-spacer></v-spacer>
-
+      <search />
       <v-btn icon>
         <v-icon>mdi-magnify</v-icon>
-      </v-btn>
-
-      <v-btn icon>
-        <v-icon>mdi-heart</v-icon>
-      </v-btn>
-
-      <v-btn icon>
-        <v-icon>mdi-dots-vertical</v-icon>
       </v-btn>
     </v-app-bar>
 
@@ -77,16 +47,19 @@
 </template>
 
 <script>
-import SnackBar from './components/Shared/SnackBar.vue'
-  export default {
-    data: () => ({ 
-      drawer: null,
-      items: [
-          { title: 'Todo', icon: 'mdi-format-list-checks', to: '/' },
-          { title: 'About', icon: 'mdi-help-box', to: '/about' },
-        ]}),
-    components : {
-      'snackbar' : require('@/components/Shared/SnackBar.vue').default
-    }
-  }
+import SnackBar from "./components/Shared/SnackBar.vue";
+import Search from "./components/Tools/Search.vue";
+export default {
+  data: () => ({
+    drawer: null,
+    items: [
+      { title: "Todo", icon: "mdi-format-list-checks", to: "/" },
+      { title: "About", icon: "mdi-help-box", to: "/about" },
+    ],
+  }),
+  components: {
+    search: require("@/components/Tools/Search.vue").default,
+    snackbar: require("@/components/Shared/SnackBar.vue").default,
+  },
+};
 </script>
---
+++ src/components/Tools/Search.vue
@@ -0,0 +1,10 @@
+<template>
+  <v-text-field label="Filled" filled></v-text-field>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
